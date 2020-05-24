d6e2fcf5938bee584f433bb0e043a313093a42ad
Eduka <eduardo.dalirio@googlemail.com>
2020-05-24 14:00:26 +0200

    replaced Freetype lib files with the correct runtime library files

--- Game/src/Main.cpp
+++ Game/src/Main.cpp
@@ -396,7 +396,14 @@ int main(int argc, char** argv)
 	bodies.push_back(body);
 
 	
+	//------------------------Text Rendering---------------------
+	FT_Library ft;
+	if (FT_Init_FreeType(&ft))
+		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
 
+	FT_Face face;
+	if (FT_New_Face(ft, "fonts/arial.ttf", 0, &face))
+		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
 	
 
 #pragma endregion
